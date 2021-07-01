6ca4f067099939ead64b38b40911e666cbf31a80
ozgeonec <ozgenuronec@gmail.com>
2021-07-01 23:52:35 +0300

    new icons for detail page

---
+++ src/components/action-button.js
@@ -0,0 +1,25 @@
+import React from "react";
+
+import Button from "./button";
+import Box from "./box";
+
+
+function ActionButton({ children, ...props }) {
+  return (
+    <Button
+      height="actionButton"
+      bg="white"
+      borderRadius="full"
+      px={8}
+      style={{
+        shadowColor: '#000',
+        shadowOpacity: 0.1,
+        shadowRadius: 20}}
+      {...props}
+    >
+        {children}
+    </Button>
+  );
+}
+
+export default ActionButton;
--- src/views/detail.js
+++ src/views/detail.js
@@ -1,19 +1,35 @@
-import { SafeAreaView, StatusBar, Text } from "react-native";
-import * as React from 'react';
+import { SafeAreaView, StatusBar } from "react-native";
+import * as React from "react";
 import { useFocusEffect } from "@react-navigation/native";
 import Box from "../components/box";
+import Text from "../components/Text";
+import ActionButton from "../components/action-button";
+import More from "../components/icons/More";
 
 function DetailView() {
 
   useFocusEffect(
     React.useCallback(() => {
-      StatusBar.setBarStyle('dark-content')
-    },[]));
+      StatusBar.setBarStyle("dark-content");
+    }, []));
 
   return (
-    <Box as={SafeAreaView} flex={1} p={16} bg="softGrey">
-      <Text>Details</Text>
+    <Box as={SafeAreaView}  p={16} bg="softGrey">
+      <Box>
+        <Text fontSize={32} fontWeight="bold" pl={16}>Details</Text>
+        <Text fontSize={18} fontStyle="italic" color="textLight" pl={16} mt={6}>Details</Text>
+      </Box>
+      <Box>
+        <ActionButton mr={250}>
+          <More/>
+          <Text color="textLight">Türk İşaret Dili</Text>
+        </ActionButton>
+        <ActionButton mr={360}>
+          <More />
+        </ActionButton>
+      </Box>
     </Box>
   );
 }
+
 export default DetailView;
